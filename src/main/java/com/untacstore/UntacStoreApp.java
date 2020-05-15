9713a78a9b260eb9bf03acf0c0fe0bd8004e3905
rn00n <rn00n@naver.com>
2020-05-15 15:03:29 +0900

    create project

---
+++ src/main/java/com/untacstore/UntacStoreApp.java
@@ -0,0 +1,13 @@
+package com.untacstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntacStoreApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UntacStoreApp.class, args);
+    }
+
+}
